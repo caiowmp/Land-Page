5f6931813c8256f40f2fb91474205e996b8ca5c1
caiomp <caiomp@ufba.br>
2024-05-21 13:24:47 -0300

    Fixing responsivity issues

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-FW6B3CGJ.mjs";import"./chunk-FD27W3JE.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-MQQ7UWRY.mjs";import"./chunk-FD27W3JE.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
